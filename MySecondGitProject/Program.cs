99dc6959de5246121202adc73f7597995809075a
niyazi.belenli <niyazi.belenli@ACER>
2023-04-09 16:29:38 +0300

    Add project files.

---
+++ MySecondGitProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySecondGitProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
